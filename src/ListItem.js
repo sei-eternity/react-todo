7a3f1d9bc17b0fa1d9c54ec0fd7c5f9ebb514c27
Usman Bashir <me@usmanbashir.com>
2019-08-06 13:09:42 +0300

    The Big Bang

---
+++ src/ListItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+  render() {
+    return <li>{this.props.task}</li>;
+  }
+}
+
+export default ListItem;
---
+++ src/MyList.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem';
+
+class MyList extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      toDoItems: props.theList,
+      newItem: "Hi Mick!"
+    };
+  }
+
+  clearList = (e) => {
+    this.setState({
+      toDoItems: []
+    });
+  }
+
+  addItem = (e) => {
+    e.preventDefault();
+
+    // const someObj = {name:"q", age: 0};
+
+    // const newObject = Object.assign(
+    //   {}, // results in a new merged object
+    //   someObj, // takes the old obj
+    //   { alive: false } // takes new/other obj
+    // );
+
+    // const currentToDoItems = this.state.toDoItems;
+    // currentToDoItems.push(this.state.newItem);
+    // this.setState({
+    //   toDoItems: currentToDoItems,
+    //   newItem: ""
+    // });
+
+    const { toDoItems, newItem } = this.state;
+
+    this.setState({
+      toDoItems: [...toDoItems, newItem],
+      newItem: ""
+    });
+  }
+
+  newItemChange = (e) => {
+    this.setState({
+      newItem: e.target.value
+    });
+  }
+
+  render() {
+    const allTasks = this.state.toDoItems.map((task, index) => {
+      return <ListItem task={task} key={index} />;
+    });
+
+    return (
+      <div>
+        <h1>Things I should stop procrastinating:</h1>
+        <ul>
+          {allTasks}
+        </ul>
+        <br />
+        
+        <form>
+          <input type="text"
+            placeholder="Type a task here!"
+            onChange={this.newItemChange}
+            value={this.state.newItem}
+            />
+          <button onClick={this.addItem}>Add Task!</button>
+        </form>
+
+        <hr />
+        <button 
+          onClick={this.clearList}>
+            I Finished Everything!
+        </button>
+      </div>
+    );
+  }
+}
+
+export default MyList;
